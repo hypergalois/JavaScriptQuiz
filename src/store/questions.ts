9f1252d9e49b34f0c9bb9b9dd946ba06486964b8
hypergalois <jose.delgado2@live.u-tad.com>
2024-09-29 15:05:56 +0200

    Refactor questions.ts: Add fetchQuestions function

--- src/store/questions.ts
+++ src/store/questions.ts
@@ -1,2 +1,16 @@
 import { create } from "zustand";
 import { Question } from "@/types";
+
+interface QuestionsState {
+	questions: Question[];
+	currentQuestion: number;
+	fetchQuestions: (limit: number) => Promise<void>;
+}
+
+export const useQuestionsStore = create<QuestionsState>((set) => ({
+	questions: [],
+	currentQuestion: 0,
+	fetchQuestions: async (limit: number) => {
+		console.log("fetchQuestions");
+	},
+}));
