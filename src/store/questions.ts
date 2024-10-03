43aa7cfd84d6f8f0080e262cff02fb5ba0631e63
hypergalois <jose.delgado2@live.u-tad.com>
2024-10-03 19:56:05 +0200

    Refactor useQuestionsStore to persist question data

--- src/store/questions.ts
+++ src/store/questions.ts
@@ -1,5 +1,6 @@
 import { create } from 'zustand';
 import { Question } from '@/types';
+import { persist } from 'zustand/middleware';
 
 import confetti from 'canvas-confetti';
 
@@ -12,56 +13,63 @@ interface QuestionsState {
   goPreviousQuestion: () => void;
 }
 
-export const useQuestionsStore = create<QuestionsState>(
-  (set, get) => ({
-    questions: [],
-    currentQuestion: 0,
-    fetchQuestions: async (limit: number) => {
-      const response = await fetch('http://localhost:5173/data.json');
-      const json = await response.json();
+export const useQuestionsStore = create<QuestionsState>()(
+  persist(
+    (set, get) => ({
+      questions: [],
+      currentQuestion: 0,
+      fetchQuestions: async (limit: number) => {
+        const response = await fetch(
+          'http://localhost:5173/data.json',
+        );
+        const json = await response.json();
 
-      const questions = json
-        .sort(() => Math.random() - 0.5)
-        .slice(0, limit);
+        const questions = json
+          .sort(() => Math.random() - 0.5)
+          .slice(0, limit);
 
-      set({ questions });
-    },
-    selectAnswer: (questionId: number, answerIndex: number) => {
-      set((state) => {
-        const questions = state.questions.map((question) => {
-          if (question.id === questionId) {
-            if (question.correctAnswer === answerIndex) confetti();
+        set({ questions });
+      },
+      selectAnswer: (questionId: number, answerIndex: number) => {
+        set((state) => {
+          const questions = state.questions.map((question) => {
+            if (question.id === questionId) {
+              if (question.correctAnswer === answerIndex) confetti();
 
-            return {
-              ...question,
-              userSelectedAnswer: answerIndex,
-              isCorrectUserAnswer:
-                question.correctAnswer === answerIndex,
-            };
-          }
-          return question;
+              return {
+                ...question,
+                userSelectedAnswer: answerIndex,
+                isCorrectUserAnswer:
+                  question.correctAnswer === answerIndex,
+              };
+            }
+            return question;
+          });
+
+          return { questions };
         });
+      },
+      goNextQuestion: () => {
+        const { currentQuestion, questions } = get();
 
-        return { questions };
-      });
+        const nextQuestion = currentQuestion + 1;
+
+        if (nextQuestion < questions.length) {
+          set({ currentQuestion: nextQuestion });
+        }
+      },
+      goPreviousQuestion: () => {
+        const { currentQuestion } = get();
+
+        const previousQuestion = currentQuestion - 1;
+
+        if (previousQuestion >= 0) {
+          set({ currentQuestion: previousQuestion });
+        }
+      },
+    }),
+    {
+      name: 'questions',
     },
-    goNextQuestion: () => {
-      const { currentQuestion, questions } = get();
-
-      const nextQuestion = currentQuestion + 1;
-
-      if (nextQuestion < questions.length) {
-        set({ currentQuestion: nextQuestion });
-      }
-    },
-    goPreviousQuestion: () => {
-      const { currentQuestion } = get();
-
-      const previousQuestion = currentQuestion - 1;
-
-      if (previousQuestion >= 0) {
-        set({ currentQuestion: previousQuestion });
-      }
-    },
-  }),
+  ),
 );
