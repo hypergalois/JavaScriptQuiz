3a495ec28fd56dc8613319cb399f3f73fc4c9892
hypergalois <jose.delgado2@live.u-tad.com>
2024-10-03 05:13:00 +0200

    Refactor fetchQuestions function in Start.tsx

--- src/store/questions.ts
+++ src/store/questions.ts
@@ -4,64 +4,39 @@ import { Question } from '@/types';
 interface QuestionsState {
   questions: Question[];
   currentQuestion: number;
-  fetchQuestions: (
-    limit: number,
-  ) => Promise<void>;
-  selectAnswer: (
-    questionId: number,
-    answerIndex: number,
-  ) => void;
+  fetchQuestions: (limit: number) => Promise<void>;
+  selectAnswer: (questionId: number, answerIndex: number) => void;
 }
 
-export const useQuestionsStore =
-  create<QuestionsState>(
-    (set, get) => ({
-      questions: [],
-      currentQuestion: 0,
-      fetchQuestions: async (
-        limit: number,
-      ) => {
-        const response = await fetch(
-          'http://localhost:5173/data.json',
-        );
-        const json =
-          await response.json();
+export const useQuestionsStore = create<QuestionsState>(
+  (set, get) => ({
+    questions: [],
+    currentQuestion: 0,
+    fetchQuestions: async (limit: number) => {
+      const response = await fetch('http://localhost:5173/data.json');
+      const json = await response.json();
 
-        const questions = json
-          .sort(
-            () => Math.random() - 0.5,
-          )
-          .slice(0, limit);
+      const questions = json
+        .sort(() => Math.random() - 0.5)
+        .slice(0, limit);
 
-        set({ questions });
-      },
-      selectAnswer: (
-        questionId: number,
-        answerIndex: number,
-      ) => {
-        set((state) => {
-          const questions =
-            state.questions.map(
-              (question) => {
-                if (
-                  question.id ===
-                  questionId
-                ) {
-                  return {
-                    ...question,
-                    selectedAnswer:
-                      answerIndex,
-                    isCorrectAnswer:
-                      question.correctAnswer ===
-                      answerIndex,
-                  };
-                }
-                return question;
-              },
-            );
-
-          return { questions };
+      set({ questions });
+    },
+    selectAnswer: (questionId: number, answerIndex: number) => {
+      set((state) => {
+        const questions = state.questions.map((question) => {
+          if (question.id === questionId) {
+            return {
+              ...question,
+              selectedAnswer: answerIndex,
+              isCorrectAnswer: question.correctAnswer === answerIndex,
+            };
+          }
+          return question;
         });
-      },
-    }),
-  );
+
+        return { questions };
+      });
+    },
+  }),
+);
