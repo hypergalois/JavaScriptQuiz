e23181981d132d9f926a6126c04413594ebd542f
hypergalois <jose.delgado2@live.u-tad.com>
2024-10-03 04:42:32 +0200

    Refactor import statements and formatting in Game.tsx

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -1,12 +1,22 @@
-import { Typography } from "@mui/material";
-import { useQuestionsStore } from "../store/questions";
-import Question from "./Question";
+import { Typography } from '@mui/material';
+import { useQuestionsStore } from '../store/questions';
+import Question from './Question';
 
 export default function Game() {
-  const questions = useQuestionsStore((state) => state.questions);
-  const currentQuestion = useQuestionsStore((state) => state.currentQuestion);
+  const questions = useQuestionsStore(
+    (state) => state.questions,
+  );
+  const currentQuestion =
+    useQuestionsStore(
+      (state) => state.currentQuestion,
+    );
 
-  const questionInfo = questions[currentQuestion];
+  const questionInfo =
+    questions[currentQuestion];
 
-  return <Question info={questionInfo} />;
+  return (
+    <>
+      <Question info={questionInfo} />
+    </>
+  );
 }
