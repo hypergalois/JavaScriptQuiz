d9040c510fc423ae960576d038c1a4d8f1528d8d
hypergalois <jose.delgado2@live.u-tad.com>
2024-10-03 04:42:19 +0200

    Refactor fetchQuestions function in useQuestionsStore

--- src/store/questions.ts
+++ src/store/questions.ts
@@ -1,21 +1,35 @@
-import { create } from "zustand";
-import { Question } from "@/types";
+import { create } from 'zustand';
+import { Question } from '@/types';
 
 interface QuestionsState {
-	questions: Question[];
-	currentQuestion: number;
-	fetchQuestions: (limit: number) => Promise<void>;
+  questions: Question[];
+  currentQuestion: number;
+  fetchQuestions: (
+    limit: number,
+  ) => Promise<void>;
 }
 
-export const useQuestionsStore = create<QuestionsState>((set, get) => ({
-	questions: [],
-	currentQuestion: 0,
-	fetchQuestions: async (limit: number) => {
-		const response = await fetch("http://localhost:5173/data.json")
-		const json = await response.json()
+export const useQuestionsStore =
+  create<QuestionsState>(
+    (set, get) => ({
+      questions: [],
+      currentQuestion: 0,
+      fetchQuestions: async (
+        limit: number,
+      ) => {
+        const response = await fetch(
+          'http://localhost:5173/data.json',
+        );
+        const json =
+          await response.json();
 
-		const questions = json.sort(() => Math.random() - 0.5).slice(0, limit)
-		
-		set({ questions })
-	},
-}));
+        const questions = json
+          .sort(
+            () => Math.random() - 0.5,
+          )
+          .slice(0, limit);
+
+        set({ questions });
+      },
+    }),
+  );
