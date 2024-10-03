f5bf594a29912a2cf2bc56e24feb4c184f2b322c
hypergalois <jose.delgado2@live.u-tad.com>
2024-10-03 04:42:26 +0200

    Refactor import statements and formatting in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
-import "@/App.css";
-import JavaScriptLogo from "@/components/JavaScriptLogo";
-import { Container, Stack, Typography } from "@mui/material";
-import Start from "@/components/Start";
-import { useQuestionsStore } from "@/store/questions";
-import Game from "./components/Game";
+import '@/App.css';
+import JavaScriptLogo from '@/components/JavaScriptLogo';
+import {
+  Container,
+  Stack,
+  Typography,
+} from '@mui/material';
+import Start from '@/components/Start';
+import { useQuestionsStore } from '@/store/questions';
+import Game from './components/Game';
 
 function App() {
-  const questions = useQuestionsStore((state) => state.questions);
+  const questions = useQuestionsStore(
+    (state) => state.questions,
+  );
   console.log(questions);
 
   return (
@@ -15,17 +21,25 @@ function App() {
         <Stack
           direction="row"
           gap={2}
-          alignItems={"center"}
-          justifyContent={"center"}
+          alignItems={'center'}
+          justifyContent={'center'}
+          paddingBottom={4}
         >
           <JavaScriptLogo />
-          <Typography variant="h2" component="h1">
+          <Typography
+            variant="h2"
+            component="h1"
+          >
             JavaScript Quiz
           </Typography>
         </Stack>
 
-        {questions.length === 0 && <Start />}
-        {questions.length > 0 && <Game />}
+        {questions.length === 0 && (
+          <Start />
+        )}
+        {questions.length > 0 && (
+          <Game />
+        )}
       </Container>
     </main>
   );
