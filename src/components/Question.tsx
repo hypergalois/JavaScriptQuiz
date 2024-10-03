5886c73f51196a32e86750dfcb93dbbca2a29e59
hypergalois <jose.delgado2@live.u-tad.com>
2024-10-03 04:42:35 +0200

    Refactor import statements and formatting in Question.tsx

--- src/components/Question.tsx
+++ src/components/Question.tsx
@@ -1,10 +1,57 @@
-import { Button, Card, Typography } from "@mui/material";
-import { type Question as QuestionType } from "../types";
+import {
+  Button,
+  Card,
+  Typography,
+  List,
+  ListItem,
+} from '@mui/material';
+import { type Question as QuestionType } from '../types';
 
-const Question = ({ info }: { info: QuestionType }) => {
+import SyntaxHighlighter from 'react-syntax-highlighter';
+import { gradientDark } from 'react-syntax-highlighter/dist/esm/styles/hljs';
+
+const Question = ({
+  info,
+}: {
+  info: QuestionType;
+}) => {
   return (
-    <Card variant="outlined">
-      <Typography variant="h5">{info.question}</Typography>
+    <Card
+      variant="outlined"
+      sx={{
+        textAlign: 'left',
+        bgcolor: '#222',
+        padding: 2,
+      }}
+    >
+      <Typography variant="h5">
+        {info.question}
+      </Typography>
+
+      <SyntaxHighlighter
+        language="javascript"
+        style={gradientDark}
+      >
+        {info.code}
+      </SyntaxHighlighter>
+
+      <List sx={{ bgcolor: '#333' }}>
+        {info.answers.map(
+          (answer, index) => (
+            <ListItem key={index}>
+              <Button
+                variant="contained"
+                sx={{
+                  width: '100%',
+                  bgcolor: '#444',
+                }}
+              >
+                {answer}
+              </Button>
+            </ListItem>
+          ),
+        )}
+      </List>
     </Card>
   );
 };
